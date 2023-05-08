84944eb637d584aab3ecd19cd34e7988504ae32e
eilon <eilon343@gmail.com>
2023-05-08 17:01:28 +0300

    Changing frontend host address to published server

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://chat-app-api-wqkh.onrender.com";
 
 //exporting user api routes
 export const registerRoute = `${host}/api/auth/register`;
